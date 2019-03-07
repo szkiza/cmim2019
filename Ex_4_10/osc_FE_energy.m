cb41901828bbee37abb40f64b97466614f02a8aa
szkiza <35806147+szkiza@users.noreply.github.com>
2019-03-07 16:22:31 +0200

    Add files via upload

---
+++ Ex_4_10/osc_FE_energy.m
@@ -0,0 +1,29 @@
+% Zalán Hodruszki --- 06/03/2019
+% Ex. 4.10
+
+omega = 2;          % natural frequency of the oscillating system
+P = 2*pi/omega;     % periodic time
+dt = P/200;         % time step
+T = 3*P;            % duartion of the simulation
+N_t = floor(T/dt);  % number of time step
+
+t = linspace(0, N_t*dt, N_t+1); % definition of the time vector
+u = zeros(N_t+1, 1);            % initialize position vector
+v = zeros(N_t+1, 1);            % initialize velocity vector
+
+% Initial condition
+X_0 = 2;    % initial position
+u(1) = X_0;
+v(1) = 0;   % initial velocity
+
+% Forward Euler method
+for n = 1:N_t
+    u(n+1) = u(n) + dt*v(n);
+    v(n+1) = v(n) - dt*omega^2*u(n);
+end
+
+% plot(t, u, 'b-', t, X_0*cos(omega*t), 'r--');
+% legend('numerical', 'exact', 'Location','northwest');
+% xlabel('t');
+
+osc_energy(u,v,t,omega); % calling the energy calculator
